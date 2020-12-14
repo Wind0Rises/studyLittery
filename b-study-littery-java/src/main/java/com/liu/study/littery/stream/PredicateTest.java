57d814486fcd984f33243094b12b2cf4d7ff6918
liuweian <1qaz2wsx>
2020-12-14 13:27:41 +0800

    Collectors 分类

---
+++ b-study-littery-java/src/main/java/com/liu/study/littery/stream/PredicateTest.java
@@ -0,0 +1,12 @@
+package com.liu.study.littery.stream;
+
+/**
+ *
+ * Predicate：函数接口测试类。
+ *
+ * @author lwa
+ * @version 1.0.0
+ * @createTime 2020/12/12 13:11
+ */
+public class PredicateTest {
+}
---
+++ b-study-littery-java/src/main/java/com/liu/study/littery/stream/SupplierTest.java
@@ -0,0 +1,11 @@
+package com.liu.study.littery.stream;
+
+/**
+ * Supplier函数接口测试类。
+ *
+ * @author lwa
+ * @version 1.0.0
+ * @createTime 2020/12/12 13:13
+ */
+public class SupplierTest {
+}
