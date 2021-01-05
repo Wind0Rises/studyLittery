3522f94bd0a129285db8b8eac2919913a8bdeeed
liuweian <1qaz2wsx>
2021-01-05 15:48:24 +0800

    MathContext开始

--- b-study-littery-java/src/main/java/com/liu/study/littery/math/StudyMathContext.java
+++ b-study-littery-java/src/main/java/com/liu/study/littery/math/StudyMathContext.java
@@ -15,6 +15,20 @@ import java.math.RoundingMode;
 public class StudyMathContext {
 
     public static void main(String[] args) {
+        createObject();
+    }
+
+    /**
+     *
+     */
+    public static void createObject() {
+        MathContext mathContext = new MathContext("precision=1 roundingMode=2");
+    }
+
+    /**
+     *
+     */
+    public static void basisMethod() {
         MathContext mathContext = new MathContext(2, RoundingMode.HALF_DOWN);
         final BigDecimal bigDecimal = new BigDecimal("56812.131212", mathContext);
         System.out.println(bigDecimal);
