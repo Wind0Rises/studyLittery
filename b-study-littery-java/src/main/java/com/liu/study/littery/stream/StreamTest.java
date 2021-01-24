ff950709c0fe5c2a90074b32470b57c0de07e4b7
liuweian <1qaz2wsx>
2021-01-24 13:23:15 +0800

    Stream 基本方法案例学习完成

--- b-study-littery-java/src/main/java/com/liu/study/littery/stream/StreamTest.java
+++ b-study-littery-java/src/main/java/com/liu/study/littery/stream/StreamTest.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,6 +16,15 @@ import java.util.stream.Stream;
  */
 public class StreamTest {
 
+
+    /**
+     * <note>
+     * 有时候需要在遍历中途停止操作，比如查找第一个满足条件的元素或者limit操作。在Stream中short-circuiting操作有：anyMatch、allMatch、noneMatch、
+     * findFirst、findAny、limit，这些操作在Sink中都有一个变量来判断是否短路，比如limit用的是m，match用的是stop，find用的是hasValue。
+     * </note>
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         /**
@@ -32,23 +38,55 @@ public class StreamTest {
         // reduceTest();
 
         /**
-         *
+         * flatMap：将一个对象Stream，然后把对象里的元素在Stream，最后操作。类似List包含List操作。
          */
         // flatMapTest();
 
         /**
-         * anyMatch()、allMatch()测试。
+         * anyMatch()、allMatch()、noonMatch()测试：判断Stream是否包含指定元素、或者是否是全部瞒住条件。
          */
          // matchTest();
 
+        /**
+         * 静态方法concat()方法：将两个stream合并成一个Stream。
+         */
+         // concatTest();
 
         /**
-         * 静态方法concat()方法。
+         * 过滤操作。
          */
-         concatTest();
+        // distinctTest();
 
+        /**
+         *
+         */
+        // findTest();
 
+        /**
+         * forEach：
+         * forEachOrdered：在parallel模式下依旧保证顺序性。
+         */
+        // forEachTest();
 
+        /**
+         *
+         */
+        // limitTest();
+
+        /**
+         *
+         */
+        // extremityTest();
+
+        /**
+         *
+         */
+        // peekTest();
+
+        /**
+         *
+         */
+        skipTest();
     }
 
     /**
@@ -169,6 +207,16 @@ public class StreamTest {
             return "1".equals(item);
         });
         System.out.println(allMatchResult);
+
+        boolean noonMatchFirst = list.stream().noneMatch(item -> {
+            return "2".equals(item);
+        });
+        System.out.println(noonMatchFirst);
+
+        boolean noonMatchSecond = list.stream().noneMatch(item -> {
+            return "12".equals(item);
+        });
+        System.out.println(noonMatchSecond);
     }
 
 
@@ -188,14 +236,120 @@ public class StreamTest {
     }
 
     /**
-     * TODO:
+     * distinct操作：
      */
     public static void distinctTest() {
         ArrayList<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
         list.add("3");
-        list.add("4");
+        list.add("2");
+        List<String> distinctResult = list.stream().distinct().collect(Collectors.toList());
+        System.out.println(distinctResult);
+    }
+
+    /**
+     * findAny：可以看到findAny()操作，返回的元素是不确定的，对于同一个列表多次调用findAny()有可能会返回不同的值。
+     *         使用findAny()是为了更高效的性能。如果是数据较少，串行地情况下，一般会返回第一个结果，如果是并行的情况，那就不能确保是第一个。
+     * findFirst：
+     */
+    public static void findTest() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("2");
+
+        String findAnyResult1 = list.stream().findAny().get();
+        String findAnyResult2 = list.stream().findAny().get();
+        String findFirstResult = list.stream().findFirst().get();
+        System.out.println("findAny：" + findAnyResult1 + "   、" +findAnyResult2);
+        System.out.println("findFirst：" + findFirstResult);
+    }
+
+    /**
+     * forEachOrdered():在并发场景下的保证顺序。
+     */
+    public static void forEachTest() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("AAA");
+        list.add("BBB");
+        list.add("CCC");
+
+        list.stream().parallel().forEach(System.out::println);
+        System.out.println("------------------------------");
+        list.stream().parallel().forEachOrdered(System.out::println);
+    }
+
+    /**
+     * limit测试：
+     */
+    public static void limitTest() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("AAA");
+        list.add("BBB");
+        list.add("CCC");
+        list.add("DDD");
+        list.add("EEE");
+        list.add("FFF");
+        list.add("GGG");
+        List<String> limitList = list.stream().limit(2).collect(Collectors.toList());
+        System.out.println(limitList);
+    }
+
+
+    /**
+     * 极限测试：min()、max()
+     */
+    public static void extremityTest() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Integer max = list.stream().max(Comparator.comparingInt(item -> item)).get();
+        Integer min = list.stream().min(Comparator.comparingInt(item -> item)).get();
+        System.out.println("MAX：" + max + "   MIN：" + min);
+    }
+
+    /**
+     * peek、foreach、map：的区别
+     *
+     * peek：主要用于Debug。
+     *
+     */
+    public static void peekTest() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("AAA");
+        list.add("BBB");
+        list.add("CCC");
+
+        List<String> peekResult = list.stream().peek(item -> {
+            item = item + "asd";
+            System.out.println("--->" + item);
+        }).collect(Collectors.toList());
+        System.out.println(peekResult);
+
+        list.stream().forEach(item -> {
+            item = item + "asd";
+            System.out.println("--->" + item);
+        });
+        System.out.println(list == peekResult);
+    }
+
+    /**
+     * skip：跳过index为0 到（n -1）元素。
+     */
+    public static void skipTest() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("AAA");
+        list.add("BBB");
+        list.add("CCC");
+        list.add("EEE");
+        list.add("FFF");
+        list.add("CCC");
+
+        List<String> skipResult = list.stream().skip(2L).collect(Collectors.toList());
+        System.out.println(skipResult);
     }
 
 
